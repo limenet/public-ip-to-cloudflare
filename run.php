26acda1423a44e747341a0c82ce29814243292cd
Linus Metzler <linus.metzler@gmail.com>
2015-10-11 15:54:16 +0200

    styleci

--- run.php
+++ run.php
@@ -2,12 +2,12 @@
 
 require 'vendor/autoload.php';
 
-
-$climate = new League\CLImate\CLImate;
+$climate = new League\CLImate\CLImate();
 
 $climate->out('--- public-ip-to-cloudflare ---');
 
 use \Curl\Curl;
+
 $curlIP = new Curl();
 $curlCF = new Curl();
 $curlCF->setOpt(CURLOPT_CAINFO, __DIR__.'/cacert.pem');
@@ -26,48 +26,48 @@ $curlCF->setHeader('X-Auth-Email', $_ENV['CLOUDFLARE_EMAIL']);
 $curlCF->setHeader('Content-Type', 'application/json');
 
 $curlCF->get('https://api.cloudflare.com/client/v4/zones',
-	[
-		'name' => $_ENV['DOMAIN']
-	]
+    [
+        'name' => $_ENV['DOMAIN'],
+    ]
 );
 
 $zoneId = $curlCF->response->result[0]->id;
 
 
 $curlCF->get('https://api.cloudflare.com/client/v4/zones/'.$zoneId.'/dns_records',
-	[
-		'name' => $_ENV['SUBDOMAIN']
-	]
+    [
+        'name' => $_ENV['SUBDOMAIN'],
+    ]
 );
 
 $recordId = $curlCF->response->result[0]->id;
 
 $curlCF->put('https://api.cloudflare.com/client/v4/zones/'.$zoneId.'/dns_records/'.$recordId,
-	[
-		'name'      => $_ENV['SUBDOMAIN'],
-		'zone_name' => $_ENV['DOMAIN'],
-		'content'   => $publicIp,
-		'type'      => $_ENV['RECORD_TYPE']
-	]
+    [
+        'name'      => $_ENV['SUBDOMAIN'],
+        'zone_name' => $_ENV['DOMAIN'],
+        'content'   => $publicIp,
+        'type'      => $_ENV['RECORD_TYPE'],
+    ]
 );
 
 if ($curlCF->response->success) {
-	$climate->green('Successfully updated '.$_ENV['SUBDOMAIN']);
+    $climate->green('Successfully updated '.$_ENV['SUBDOMAIN']);
 } else {
-	$climate->error('Failed to update '.$_ENV['SUBDOMAIN']);
-	$errors = [];
-	$i = 0;
-	foreach($curlCF->response->errors as $error) {
-		$errors[$i] = $error->message.': ';
-		$chains     = [];
+    $climate->error('Failed to update '.$_ENV['SUBDOMAIN']);
+    $errors = [];
+    $i = 0;
+    foreach($curlCF->response->errors as $error) {
+        $errors[$i] = $error->message.': ';
+        $chains     = [];
 
-		foreach ($error->error_chain as $chain) {
-			$chains[] = $chain->message;
-		}
+        foreach ($error->error_chain as $chain) {
+            $chains[] = $chain->message;
+        }
 
-		$errors[$i] .= implode(', ', $chains);
-		$i++;
-	}
+        $errors[$i] .= implode(', ', $chains);
+        $i++;
+    }
 
-	$climate->error($errors);
+    $climate->error($errors);
 }
