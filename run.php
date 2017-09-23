4153404913e2834c879d3ceb444a19f471bf158a
Linus Metzler <hi@linusmetzler.me>
2017-09-23 10:54:14 +0000

    Apply fixes from StyleCI

--- run.php
+++ run.php
@@ -15,41 +15,40 @@ $climate = new League\CLImate\CLImate();
 
 $climate->out('--- public-ip-to-cloudflare ---');
 
-use \Curl\Curl;
+use Curl\Curl;
 
 $curlIP = new Curl();
 
-$cfKey     = new \Cloudflare\API\Auth\APIKey($_ENV['CLOUDFLARE_EMAIL'], $_ENV['CLOUDFLARE_KEY']);
+$cfKey = new \Cloudflare\API\Auth\APIKey($_ENV['CLOUDFLARE_EMAIL'], $_ENV['CLOUDFLARE_KEY']);
 $cfAdapter = new Cloudflare\API\Adapter\Guzzle($cfKey);
-$cfDns    = new \Cloudflare\API\Endpoints\DNS($cfAdapter);
-$cfZone    = new \Cloudflare\API\Endpoints\Zones($cfAdapter);
-
+$cfDns = new \Cloudflare\API\Endpoints\DNS($cfAdapter);
+$cfZone = new \Cloudflare\API\Endpoints\Zones($cfAdapter);
 
 $publicIp = $curlIP->get('http://ipecho.net/plain');
 
 $climate->out('Your public IP address is '.$publicIp);
 
 try {
-$cfCall = $cfZone->getZoneID($_ENV['DOMAIN']);
-$zoneId = $cfCall;
+    $cfCall = $cfZone->getZoneID($_ENV['DOMAIN']);
+    $zoneId = $cfCall;
 
-$cfCall = $cfDns->listRecords($zoneId, '', $_ENV['SUBDOMAIN']);
-if (count($cfCall->result) !== 1) {
-    $climate->error(sprintf('Record %2$s not found in zone %1$s', $_ENV['DOMAIN'], $_ENV['SUBDOMAIN']));
-    $climate->error('Please first add the record manually. This will be fixed in a future version.');
-    die();
-}
+    $cfCall = $cfDns->listRecords($zoneId, '', $_ENV['SUBDOMAIN']);
+    if (count($cfCall->result) !== 1) {
+        $climate->error(sprintf('Record %2$s not found in zone %1$s', $_ENV['DOMAIN'], $_ENV['SUBDOMAIN']));
+        $climate->error('Please first add the record manually. This will be fixed in a future version.');
+        die();
+    }
 
-$recordId = $cfCall->result[0]->id;
+    $recordId = $cfCall->result[0]->id;
 
-$cfCall = $cfDns->updateRecordDetails($zoneId, $recordId, [
+    $cfCall = $cfDns->updateRecordDetails($zoneId, $recordId, [
         'type'      => $_ENV['RECORD_TYPE'],
         'content'   => $publicIp,
         'proxiable' => true,
         'proxied'   => $_ENV['PROXIED'],
     ]
 );
-}catch(Exception $e){
+} catch (Exception $e) {
     $climate->error('Failed to update '.$_ENV['SUBDOMAIN']);
     $climate->error($e->getMessage());
     if (property_exists($cfCall, 'errors')) {
